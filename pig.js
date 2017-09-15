250f181925f231b2ea9126d5e91923c80dd8fa87
dkapper01 <dkapper01@gmail.com>
2017-09-15 12:34:20 -0500

    add jquery file

--- pig.js
+++ pig.js
@@ -1,10 +1,16 @@
 $('.roll-btn').click(function() {
   $( ".toggle" ).effect( "shake" );
 });
+var dice0 = 0; 
+var dice1 = 0;
 
-var dice0 = Math.floor(Math.random() * 6) + 1;
-var dice1 = Math.floor(Math.random() * 6) + 1; 
+console.log(dice0);
 
 document.querySelector(".roll-btn").addEventListener("click", function() {
 
+	var dice0 = Math.floor(Math.random() * 6) + 1;
+	var dice1 = Math.floor(Math.random() * 6) + 1; 
+	
+	document.querySelector('.dice0').src = 'img/dice-' + dice0 + '.png';  
+	document.querySelector('.dice1').src = 'img/dice-' + dice1 + '.png'; 
 });
