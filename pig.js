b05a8e9b6aa9511674f878b95f03b2963aa536b3
dkapper01 <dkapper01@gmail.com>
2017-09-15 17:09:00 -0500

    hold btn make current score = score

--- pig.js
+++ pig.js
@@ -4,6 +4,8 @@ $('.roll-btn').click(function() {
 
 var dice0 = 0; 
 var dice1 = 0;
+var currentScore = 0; 
+var score = 0; 
 
 document.querySelector(".roll-btn").addEventListener("click", function() {
 
@@ -13,8 +15,25 @@ document.querySelector(".roll-btn").addEventListener("click", function() {
 	document.querySelector('.dice0').src = 'img/dice-' + dice0 + '.png';  
 	document.querySelector('.dice1').src = 'img/dice-' + dice1 + '.png'; 
 
-	document.getElementById('player-0-current').textContent = dice0; 
+	currentScore += dice0 + dice1; 
+
+	document.getElementById('player-0-current').textContent = currentScore; 
+
+
 });
+
+document.querySelector('.hold-btn').addEventListener("click", function() {
+	score = currentScore;  
+
+	document.getElementById('player-0-score').textContent = score; 
+	currentScore = 0; 
+	document.getElementById('player-0-current').textContent = currentScore;
+
+	//next player  
+	
+
+});
+
 
 document.getElementById('player-0-current').textContent = "0"; 
 document.getElementById('player-1-current').textContent = "0"; 
