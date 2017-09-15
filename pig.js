d94f0bd5b021a0f74c4c71b09f0f957143d15da5
dkapper01 <dkapper01@gmail.com>
2017-09-14 23:59:32 -0500

    add dice js

--- pig.js
+++ pig.js
@@ -2,3 +2,9 @@ $('.roll-btn').click(function() {
   $( ".toggle" ).effect( "shake" );
 });
 
+var dice0 = Math.floor(Math.random() * 6) + 1;
+var dice1 = Math.floor(Math.random() * 6) + 1; 
+
+document.querySelector(".roll-btn").addEventListener("click", function() {
+
+});
