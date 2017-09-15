529e64852789035b7044409f21cdc798ccfa393c
dkapper01 <dkapper01@gmail.com>
2017-09-15 16:46:07 -0500

    set scores to 0

--- pig.js
+++ pig.js
@@ -5,8 +5,6 @@ $('.roll-btn').click(function() {
 var dice0 = 0; 
 var dice1 = 0;
 
-console.log(dice0);
-
 document.querySelector(".roll-btn").addEventListener("click", function() {
 
 	var dice0 = Math.floor(Math.random() * 6) + 1;
@@ -14,4 +12,11 @@ document.querySelector(".roll-btn").addEventListener("click", function() {
 	
 	document.querySelector('.dice0').src = 'img/dice-' + dice0 + '.png';  
 	document.querySelector('.dice1').src = 'img/dice-' + dice1 + '.png'; 
+
+	document.getElementById('player-0-current').textContent = dice0; 
 });
+
+document.getElementById('player-0-current').textContent = "0"; 
+document.getElementById('player-1-current').textContent = "0"; 
+document.getElementById('player-0-score').textContent = "0"; 
+document.getElementById('player-1-score').textContent = "0"; 
